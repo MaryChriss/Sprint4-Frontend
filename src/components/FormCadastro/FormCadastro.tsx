d9ac883eb3d1143dfac4db9d0789349e7fea1b1d
Mariana <marychris.crf@gmail.com>
2024-11-03 02:07:56 -0300

    feat: adicionando API para login e cadastro

--- src/components/FormCadastro/FormCadastro.tsx
+++ src/components/FormCadastro/FormCadastro.tsx
@@ -1,72 +1,148 @@
-import React from "react";
-import { useForm } from "react-hook-form";
+import { useState } from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as Yup from "yup";
-import { Button, Input, SwitchLink } from "../FormLogin/FormLogin.style";
+import { FaArrowLeft } from "react-icons/fa";
 import { Styledh1Cadas } from "./FormCadastro.style";
-import { FaCircleArrowLeft } from "react-icons/fa6";
+import { Button, Input, SwitchLink } from "../FormLogin/FormLogin.style";
+
+interface ClienteFormData {
+    nome_cliente: string;
+    email: string;
+    endereco: string;
+    telefone: string;
+}
+
+interface LoginFormData {
+    password: string;
+    confirmPassword: string;
+}
 
 interface FormCadastroProps {
     toggleForm: () => void;
 }
 
-const schema = Yup.object().shape({
-    name: Yup.string().required("Nome é obrigatório"),
+const schemaCliente = Yup.object().shape({
+    nome_cliente: Yup.string().required("Nome é obrigatório"),
     email: Yup.string().email("Email inválido").required("Email é obrigatório"),
+    endereco: Yup.string().required("Endereço é obrigatório"),
+    telefone: Yup.string().required("Telefone é obrigatório"),
+});
+
+const schemaLogin = Yup.object().shape({
     password: Yup.string().min(6, "A senha deve ter pelo menos 6 caracteres").required("Senha é obrigatória"),
     confirmPassword: Yup.string()
-        .oneOf([Yup.ref("password"), null], "As senhas não coincidem")
+        .oneOf([Yup.ref("password")], "As senhas não coincidem")
         .required("Confirme sua senha"),
 });
 
 const FormCadastro: React.FC<FormCadastroProps> = ({ toggleForm }) => {
-    const { register, handleSubmit, formState: { errors } } = useForm({
-        resolver: yupResolver(schema),
-    });
+    const [step, setStep] = useState(1); 
+    const [clienteId, setClienteId] = useState<string | null>(null);
+    const [email, setEmail] = useState<string>(""); 
+    const apiUrl = process.env.NEXT_PUBLIC_API_URL;
 
-    const onSubmit = (data: any) => {
-        const { name, email, password } = data;
-        localStorage.setItem("user", JSON.stringify({ name, email, password }));
-        alert("Cadastro realizado com sucesso! Agora faça o login.");
-        toggleForm();
+    const {
+        register: registerCliente,
+        handleSubmit: handleSubmitCliente,
+        formState: { errors: errorsCliente },
+    } = useForm<ClienteFormData>({ resolver: yupResolver(schemaCliente) });
+
+    const {
+        register: registerLogin,
+        handleSubmit: handleSubmitLogin,
+        formState: { errors: errorsLogin },
+        reset: resetLoginForm,
+    } = useForm<LoginFormData>({ resolver: yupResolver(schemaLogin), defaultValues: {password: "", confirmPassword: ""} });
+
+    const onSubmitCliente: SubmitHandler<ClienteFormData> = async (login) => {
+        const { nome_cliente, email, endereco, telefone } = login;
+        setEmail(email); 
+    
+        try {
+            const response = await fetch(`${apiUrl}/webapi/cliente`, {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({
+                    nome_cliente: nome_cliente,
+                    email: email,
+                    endereco: endereco,
+                    telefone: telefone,
+                }),
+            });
+    
+            if (response.ok) {
+                const result = await response.json();
+                setClienteId(result.id_cliente);
+                setEmail(result.email);
+
+                resetLoginForm();
+                setStep(2);
+            } else {
+                console.log("erro",response.json);
+                console.log("erro",response);
+                alert("Erro ao cadastrar. Verifique os dados e tente novamente.");
+            }
+        } catch (error) {
+            console.error("Erro ao chamar a API:", error);
+            alert("Erro ao cadastrar. Por favor, tente novamente mais tarde.");
+        }
+    };
+
+    const onSubmitLogin: SubmitHandler<LoginFormData> = async (data) => {
+        const { password } = data;
+        try {
+            await fetch(`${apiUrl}/webapi/login`, {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({
+                    email_login: email ,
+                    senha_login: password,
+                    id_cliente: clienteId ,
+                }),
+            });
+
+            alert("Cadastro realizado com sucesso! Agora faça o login.");
+            toggleForm();
+        } catch (error) {
+            console.error("Erro ao chamar a API:", error);
+            alert("Erro ao cadastrar. Por favor, tente novamente mais tarde.");
+        }
     };
 
     return (
         <>
-            <FaCircleArrowLeft size={25} onClick={toggleForm}/>
+            <FaArrowLeft size={25} onClick={toggleForm} />
             <Styledh1Cadas>Cadastre-se:</Styledh1Cadas>
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <Input
-                    type="text"
-                    placeholder="Nome Completo"
-                    {...register("name")}
-                />
-                {errors.name && <p>{errors.name.message}</p>}
+            {step === 1 ? (
+                <form onSubmit={handleSubmitCliente(onSubmitCliente)}>
+                    <Input type="text" placeholder="Nome Completo" {...registerCliente("nome_cliente")} />
+                    {errorsCliente.nome_cliente && <p>{errorsCliente.nome_cliente.message}</p>}
 
-                <Input
-                    type="email"
-                    placeholder="Email"
-                    {...register("email")}
-                />
-                {errors.email && <p>{errors.email.message}</p>}
+                    <Input type="email" placeholder="Email" {...registerCliente("email")} />
+                    {errorsCliente.email && <p>{errorsCliente.email.message}</p>}
 
-                <Input
-                    type="password"
-                    placeholder="Senha"
-                    {...register("password")}
-                />
-                {errors.password && <p>{errors.password.message}</p>}
+                    <Input type="text" placeholder="Endereço" {...registerCliente("endereco")} />
+                    {errorsCliente.endereco && <p>{errorsCliente.endereco.message}</p>}
 
-                <Input
-                    type="password"
-                    placeholder="Confirme a senha"
-                    {...register("confirmPassword")}
-                />
-                {errors.confirmPassword && <p>{errors.confirmPassword.message}</p>}
+                    <Input type="text" placeholder="Telefone" {...registerCliente("telefone")} />
+                    {errorsCliente.telefone && <p>{errorsCliente.telefone.message}</p>}
 
-                <Button type="submit">Cadastrar</Button>
-            </form>
+                    <Button type="submit">Próximo</Button>
+                </form>
+            ) : (
+                <form onSubmit={handleSubmitLogin(onSubmitLogin)}>
 
+                    
+                    <Input type="password" placeholder="Senha" {...registerLogin("password")} />
+                    {errorsLogin.password && <p>{errorsLogin.password.message}</p>}
+
+                    <Input type="password" placeholder="Confirme sua senha" {...registerLogin("confirmPassword")} />
+                    {errorsLogin.confirmPassword && <p>{errorsLogin.confirmPassword.message}</p>}
+
+                    <Button type="submit">Cadastrar Login</Button>
+                </form>
+            )}
             <SwitchLink onClick={toggleForm}>Já tem uma conta? Login</SwitchLink>
         </>
     );
--- src/components/FormLogin/FormLogin.tsx
+++ src/components/FormLogin/FormLogin.tsx
@@ -1,16 +1,23 @@
 import React from "react";
-import { useForm } from "react-hook-form";
+import { useForm, SubmitHandler } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as Yup from "yup";
 import { BiUser } from "react-icons/bi";
-import { FaLock } from "react-icons/fa";
 import { Button, Input, InputContainer, InputIcon, Styledh1, SwitchLink } from "./FormLogin.style";
 import { useRouter } from "next/navigation";
-import { FaCircleArrowLeft } from "react-icons/fa6";
+import { FaCircleArrowLeft, FaLock } from "react-icons/fa6";
+
+const apiUrl = process.env.NEXT_PUBLIC_API_URL;
 
 interface FormLoginProps {
     toggleForm: () => void;
 }
+
+interface LoginFormData {
+    email: string;
+    password: string;
+}
+
 
 const schema = Yup.object().shape({
     email: Yup.string().email("Email inválido").required("Email é obrigatório"),
@@ -19,29 +26,38 @@ const schema = Yup.object().shape({
 
 const FormLogin: React.FC<FormLoginProps> = ({ toggleForm }) => {
     const router = useRouter();
-    const { register, handleSubmit, formState: { errors } } = useForm({
+    const { register, handleSubmit, formState: { errors } } = useForm<LoginFormData>({
         resolver: yupResolver(schema),
     });
 
-    const onSubmit = (data: any) => {
-        const savedUser = localStorage.getItem("user");
-        if (savedUser) {
-            const { email: savedEmail, password: savedPassword } = JSON.parse(savedUser);
+    const onSubmit: SubmitHandler<LoginFormData> = async (data) => {
+        try {
+            const response = await fetch(`${apiUrl}/webapi/login/autenticar`, {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({
+                    email_login: data.email,
+                    senha_login: data.password,
+                }),
+            });
 
-            if (data.email === savedEmail && data.password === savedPassword) {
+            if (response.status === 202) {
                 alert("Login bem-sucedido!");
-                router.push("/perfil");
-            } else {
+                router.push("/perfil"); // Redireciona para a página de perfil
+            } else if (response.status === 403) {
                 alert("E-mail ou senha incorretos.");
+            } else {
+                alert("Erro ao autenticar. Tente novamente mais tarde.");
             }
-        } else {
-            alert("Nenhum usuário cadastrado com este e-mail.");
+        } catch (error) {
+            console.error("Erro ao chamar a API:", error);
+            alert("Erro ao autenticar. Por favor, tente novamente mais tarde.");
         }
     };
 
     return (
         <>
-            <FaCircleArrowLeft size={25} onClick={() => router.push('/')}/>
+            <FaCircleArrowLeft size={25} onClick={() => router.push('/')} />
             <Styledh1>Login:</Styledh1>
             <form onSubmit={handleSubmit(onSubmit)}>
                 <InputContainer>
