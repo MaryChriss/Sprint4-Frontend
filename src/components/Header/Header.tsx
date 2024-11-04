5b2a86192ef642e337a3beea78329d1db0e28b6d
Mariana <marychris.crf@gmail.com>
2024-11-03 23:33:05 -0300

    Adicionado Logout

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,48 +1,67 @@
-import { BiGroup, BiHome, BiUserCircle } from "react-icons/bi"
-import { LogoPorto } from "../LogoPorto/LogoPorto"
-import { StyledHeader, StyledLink, StyledLogin, StyledMenuItem, StyledRotas, StyledWrapperMenu } from "./Header.style"
-import { BsTelephone } from "react-icons/bs"
-import { LuUserSquare2 } from "react-icons/lu"
+"use client";
 
-export const Header =() => {
+import { useEffect, useState } from "react";
+import { BiGroup, BiHome, BiUserCircle, BiLogOut } from "react-icons/bi";
+import { LogoPorto } from "../LogoPorto/LogoPorto";
+import { StyledHeader, StyledLink, StyledLogin, StyledMenuItem, StyledRotas, StyledWrapperMenu } from "./Header.style";
+import { BsTelephone } from "react-icons/bs";
+import { LuUserSquare2 } from "react-icons/lu";
+import { useRouter } from "next/navigation"; // Utilize o `next/navigation` para Next.js 13
+
+export const Header = () => {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const router = useRouter();
+
+    useEffect(() => {
+        if (typeof window !== "undefined") {
+            const loginData = localStorage.getItem("loginData");
+            setIsLoggedIn(!!loginData);
+        }
+    }, []);
+
+    const handleLogout = () => {
+        localStorage.removeItem("loginData");
+        setIsLoggedIn(false);
+        router.push("/");
+    };
+
     return (
-        <>
         <StyledHeader>
-
             <LogoPorto />
+            <StyledRotas>
+                <StyledWrapperMenu>
+                    <StyledMenuItem>
+                        <BiHome size="1.5rem" color="#00a1fc" />
+                        <StyledLink href="/">Home</StyledLink>
+                    </StyledMenuItem>
+                    <StyledMenuItem>
+                        <BiGroup size="1.5rem" color="#00a1fc" />
+                        <StyledLink href="/equipe">Equipe</StyledLink>
+                    </StyledMenuItem>
+                    <StyledMenuItem>
+                        <BsTelephone size="1.2rem" color="#00a1fc" />
+                        <StyledLink href="/contato">Contato</StyledLink>
+                    </StyledMenuItem>
+                    <StyledMenuItem>
+                        <LuUserSquare2 size="1.5rem" color="#00a1fc" />
+                        <StyledLink href="/perfil">Perfil</StyledLink>
+                    </StyledMenuItem>
+                </StyledWrapperMenu>
+            </StyledRotas>
 
-                <StyledRotas>
-                    <StyledWrapperMenu>
-
-                        <StyledMenuItem>
-                            <BiHome size="1.5rem" color="#00a1fc" />
-                            <StyledLink href="/">Home</StyledLink>
-                        </StyledMenuItem>
-
-                        <StyledMenuItem>
-                            <BiGroup size="1.5rem" color="#00a1fc" />
-                            <StyledLink href="/equipe">Equipe</StyledLink>
-                        </StyledMenuItem>
-
-                        <StyledMenuItem>
-                            <BsTelephone size="1.2rem" color="#00a1fc" />
-                            <StyledLink href="/contato">Contato</StyledLink>
-                        </StyledMenuItem>
-
-                        <StyledMenuItem>
-                            <LuUserSquare2 size="1.5rem" color="#00a1fc" />
-                            <StyledLink href="/perfil">Perfil</StyledLink>
-                        </StyledMenuItem>
-                        
-                    </StyledWrapperMenu>
-                </StyledRotas>
-
-                <StyledLogin>
-                    <BiUserCircle size="1.5rem" color="#00a1fc" />
-                    <StyledLink href="/login">Login</StyledLink>
-                </StyledLogin>
-
+            <StyledLogin>
+                {isLoggedIn ? (
+                    <>
+                        <BiLogOut size="1.5rem" color="#00a1fc" onClick={handleLogout} style={{ cursor: "pointer" }} />
+                        <span onClick={handleLogout} style={{ cursor: "pointer", color: "#00a1fc" }}>Logout</span>
+                    </>
+                ) : (
+                    <>
+                        <BiUserCircle size="1.5rem" color="#00a1fc" />
+                        <StyledLink href="/login">Login</StyledLink>
+                    </>
+                )}
+            </StyledLogin>
         </StyledHeader>
-        </>
-    )
-}
+    );
+};
